63c3853f384ba5941efa244293b79b0e45f8eedd
Aradvári Balázs <arad@Aradvaris-MacBook-Pro.local>
2018-03-22 20:06:18 +0100

    balazs,panni merge

--- web/js/site.js
+++ web/js/site.js
@@ -111,3 +111,25 @@ $(function () {
         return false;
     } );
 });
+
+/* Customer logo */
+$('#customerLogos').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('#customerLogos.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('#customerLogos.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('#customerLogos.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
