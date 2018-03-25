7be2d608c2424d8da295ba4a00b73b5f0d66ea74
Panni Turnhauser <turnhauser.panni@gmail.com>
2018-03-25 11:11:40 +0200

    Navigation dropdown fix

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=coreshop',
     'username' => 'root',
-    'password' => 'mysql',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
