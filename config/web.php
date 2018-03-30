bebf690cba5dc396377bf6726a54a6d3d2f6e066
Aradvári Balázs <arad@Aradvaris-MacBook-Pro.local>
2018-03-30 10:54:19 +0200

    date fix

--- config/web.php
+++ config/web.php
@@ -64,7 +64,7 @@ $config = [
             ],
         ],
         'formatter' => [
-            'dateFormat' => 'yyyy.mm.dd',
+            'dateFormat' => 'long',
             'decimalSeparator' => ',',
             'thousandSeparator' => ' ',
             'currencyCode' => 'Ft',
