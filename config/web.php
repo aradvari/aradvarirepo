22b4160851c33eba386a3ea4c9a007fbe231349a
Aradvári Balázs <arad@Aradvaris-MacBook-Pro.local>
2018-03-30 11:36:33 +0200

    mailer config

--- config/web.php
+++ config/web.php
@@ -32,7 +32,7 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => YII_ENV_DEV,
+//            'useFileTransport' => YII_ENV_DEV,
             'messageConfig' => [
                 'charset' => 'UTF-8',
                 'from' => 'info@coreshop.hu',
