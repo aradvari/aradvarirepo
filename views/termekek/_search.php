1ba3e00521a043ac924748c135319982518fb5df
Aradvári Balázs <arad@Aradvaris-MacBook-Pro.local>
2018-06-20 19:49:41 +0200

    mobil search collapse

--- views/termekek/_search.php
+++ views/termekek/_search.php
@@ -17,10 +17,11 @@ use yii\helpers\ArrayHelper;
             <!--<button type="button" class="btn btn-mobile btn-filter" data-toggle="modal" data-target="#filter-modal">
               Szűrő
             </button>-->
-            <a class="btn btn-mobile btn-filter" data-toggle="collapse" href="#filter-modal" role="button" aria-expanded="false" aria-controls="filter-modal">Szűrő</a>
+            <a class="btn btn-mobile btn-filter" data-toggle="collapse" href="#filter-modal" role="button"
+               aria-expanded="false" aria-controls="filter-modal">Szűrő</a>
         </div>
         <div class="col-7">
-           <form class="clearfix" method="get" id="order-form"
+            <form class="clearfix" method="get" id="order-form"
                   action="<?= Url::to([
                       'termekek/index',
                       'mainCategory' => $params['mainCategory'],
@@ -34,26 +35,29 @@ use yii\helpers\ArrayHelper;
                     echo Html::hiddenInput('q', $params[q]);
                 ?>
                 <div class="filter-topnav justify-content-around">
- 
-                        <select name="s" class="form-control custom-select">
-                            <option value="leguljabb-elol" <?= $params['s'] == 'leguljabb-elol' ? 'selected' : '' ?>>
-                                Újdonságok
-                            </option>
-                            <option value="ar-szerint-csokkeno" <?= $params['s'] == 'ar-szerint-csokkeno' ? 'selected' : '' ?>>
-                                Ár szerint csökkenő
-                            </option>
-                            <option value="ar-szerint-novekvo" <?= $params['s'] == 'ar-szerint-novekvo' ? 'selected' : '' ?>>
-                                Ár szerint növekvő
-                            </option>
-                        </select>
+
+                    <select name="s" class="form-control custom-select">
+                        <option value="leguljabb-elol" <?= $params['s'] == 'leguljabb-elol' ? 'selected' : '' ?>>
+                            Újdonságok
+                        </option>
+                        <option value="ar-szerint-csokkeno" <?= $params['s'] == 'ar-szerint-csokkeno' ? 'selected' : '' ?>>
+                            Ár szerint csökkenő
+                        </option>
+                        <option value="ar-szerint-novekvo" <?= $params['s'] == 'ar-szerint-novekvo' ? 'selected' : '' ?>>
+                            Ár szerint növekvő
+                        </option>
+                    </select>
                 </div>
             </form>
         </div>
     </div>
 </div>
 
+<?php
+$show = $params['brand'] || $params['meret'] || $params['szin'];
+?>
 
-<div class="termekek-search collapse" id="filter-modal">
+<div class="termekek-search collapse <?= $show ? 'show' : '' ?>" id="filter-modal">
     <?php
     //    //FŐKATEGÓRIÁK
     //    $models = $mainCategoryDataProvider->getModels();
@@ -71,26 +75,26 @@ use yii\helpers\ArrayHelper;
         <?php
         //ALKATEGÓRIÁK
         if ($params['mainCategory']):
-        ?>
-        <div class="desktop-subcat">
-            <!--<h4 class="filter-name filter-name-first">Kategóriák</h4>-->
-            <div class="indent">
-                <ul class="list-unstyled">
-                    <?php
-                    $models = $subCategoryDataProvider->getModels();
-                    foreach ($models as $item) {
-                        echo "<li>";
-                        echo Html::a(
-                            $item['megnevezes'],
-                            ['termekek/index', 'mainCategory' => $item['pk_url_segment'], 'subCategory' => $item['url_segment'], 's' => $params['s']],
-                            ['class' => $params['subCategory'] == $item['url_segment'] ? 'selected' : '']
-                        );
-                        echo "</li>";
-                    }
-                    ?>
-                </ul>
+            ?>
+            <div class="desktop-subcat">
+                <!--<h4 class="filter-name filter-name-first">Kategóriák</h4>-->
+                <div class="indent">
+                    <ul class="list-unstyled">
+                        <?php
+                        $models = $subCategoryDataProvider->getModels();
+                        foreach ($models as $item) {
+                            echo "<li>";
+                            echo Html::a(
+                                $item['megnevezes'],
+                                ['termekek/index', 'mainCategory' => $item['pk_url_segment'], 'subCategory' => $item['url_segment'], 's' => $params['s']],
+                                ['class' => $params['subCategory'] == $item['url_segment'] ? 'selected' : '']
+                            );
+                            echo "</li>";
+                        }
+                        ?>
+                    </ul>
+                </div>
             </div>
-        </div>
         <?php
         endif;
         ?>
@@ -100,25 +104,25 @@ use yii\helpers\ArrayHelper;
         //MÁRKÁK
         $models = $brandDataProvider->getModels();
         if (count($models) > 0):
-        ?>
-        <div class="desktop-filter">
-            <h4 class="filter-name">Márka</h4>
-            <div class="container size-filter-container brand-container">
-                <div class="row">
-                    <?php
-                    foreach ($models as $item) {
+            ?>
+            <div class="desktop-filter">
+                <h4 class="filter-name">Márka</h4>
+                <div class="container size-filter-container brand-container">
+                    <div class="row">
+                        <?php
+                        foreach ($models as $item) {
 
-                        $active = $params['brand'] == $item['url_segment'] ? true : false;
-                        echo Html::a(
-                            $item['markanev'],
-                            ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => ($active ? null : $item['url_segment']), 'meret' => $params['meret'], 'szin' => $params['szin'], 'tipus' => $params['tipus'], 'q' => $params['q'], 's' => $params['s']],
-                            ['class' => $active ? 'sizeButtonSelected' : 'sizeButton']
-                        );
-                    }
-                    ?>
+                            $active = $params['brand'] == $item['url_segment'] ? true : false;
+                            echo Html::a(
+                                $item['markanev'],
+                                ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => ($active ? null : $item['url_segment']), 'meret' => $params['meret'], 'szin' => $params['szin'], 'tipus' => $params['tipus'], 'q' => $params['q'], 's' => $params['s']],
+                                ['class' => $active ? 'sizeButtonSelected' : 'sizeButton']
+                            );
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php
         endif;
         ?>
@@ -128,24 +132,24 @@ use yii\helpers\ArrayHelper;
         //MÉRETEK
         $models = $sizeDataProvider->getModels();
         if (count($models) > 0):
-        ?>
-        <div class="desktop-filter">
-            <h4 class="filter-name">Méret</h4>
-            <div class="container size-container">
-                <div class="row">
-                    <?php
-                    foreach ($models as $item) {
-                        $active = $params['meret'] == $item['url_segment'] ? true : false;
-                        echo Html::a(
-                            $item['megnevezes'],
-                            ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => $params['brand'], 'meret' => ($active ? null : $item['url_segment']), 'szin' => $params['szin'], 'tipus' => $params['tipus'], 'q' => $params['q'], 's' => $params['s']],
-                            ['class' => $active ? 'sizeButtonSelected' : 'sizeButton']
-                        );
-                    }
-                    ?>
+            ?>
+            <div class="desktop-filter">
+                <h4 class="filter-name">Méret</h4>
+                <div class="container size-container">
+                    <div class="row">
+                        <?php
+                        foreach ($models as $item) {
+                            $active = $params['meret'] == $item['url_segment'] ? true : false;
+                            echo Html::a(
+                                $item['megnevezes'],
+                                ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => $params['brand'], 'meret' => ($active ? null : $item['url_segment']), 'szin' => $params['szin'], 'tipus' => $params['tipus'], 'q' => $params['q'], 's' => $params['s']],
+                                ['class' => $active ? 'sizeButtonSelected' : 'sizeButton']
+                            );
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php
         endif;
         ?>
@@ -154,15 +158,15 @@ use yii\helpers\ArrayHelper;
         //SZÍNEK
         $models = $colorDataProvider->getModels();
         if (count($models) > 0):
-        ?>
-        <div class="desktop-filter">
-            <h4 class="filter-name">Szín</h4>
-            <div class="row justify-content-start color-container">
-               
-                <?php
-                foreach ($models as $item) {
-                    echo '<div class="float-left">';
-                    $active = $params['szin'] == $item['szinszuro'] ? true : false;
+            ?>
+            <div class="desktop-filter">
+                <h4 class="filter-name">Szín</h4>
+                <div class="row justify-content-start color-container">
+
+                    <?php
+                    foreach ($models as $item) {
+                        echo '<div class="float-left">';
+                        $active = $params['szin'] == $item['szinszuro'] ? true : false;
                         echo "<span>";
                         echo Html::a(
                             "",
@@ -170,11 +174,11 @@ use yii\helpers\ArrayHelper;
                             ['class' => $active ? 'colorButton colorButtonSelected ' . $item['szinszuro'] : 'colorButton ' . $item['szinszuro']]
                         );
                         echo "</span>";
-                    echo '</div>';
-                }
-                ?>
+                        echo '</div>';
+                    }
+                    ?>
+                </div>
             </div>
-        </div>
         <?php
         endif;
         ?>
@@ -183,22 +187,22 @@ use yii\helpers\ArrayHelper;
         //TÍPUSOK
         $models = $typeDataProvider->getModels();
         if (count($models) > 0):
-        ?>
-        <div class="desktop-filter">
-            <h4 class="filter-name">Típus</h4>
-            <div class="indent">
-                <?php
-                foreach ($models as $item) {
-                    $active = $params['tipus'] == $item['tipus'] ? true : false;
-                    echo Html::a(
-                        $item['tipus'],
-                        ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => $params['brand'], 'meret' => $params['meret'], 'szin' => $params['szin'], 'tipus' => ($active ? null : $item['tipus']), 'q' => $params['q'], 's' => $params['s']],
-                        ['class' => $active ? 'sizeButtonSelected col-5 col-md-5 col-lg-5 mx-1 my-1' : 'sizeButton col-5 col-md-5 col-lg-5 mx-1 my-1']
-                    );
-                }
-                ?>
+            ?>
+            <div class="desktop-filter">
+                <h4 class="filter-name">Típus</h4>
+                <div class="indent">
+                    <?php
+                    foreach ($models as $item) {
+                        $active = $params['tipus'] == $item['tipus'] ? true : false;
+                        echo Html::a(
+                            $item['tipus'],
+                            ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => $params['brand'], 'meret' => $params['meret'], 'szin' => $params['szin'], 'tipus' => ($active ? null : $item['tipus']), 'q' => $params['q'], 's' => $params['s']],
+                            ['class' => $active ? 'sizeButtonSelected col-5 col-md-5 col-lg-5 mx-1 my-1' : 'sizeButton col-5 col-md-5 col-lg-5 mx-1 my-1']
+                        );
+                    }
+                    ?>
+                </div>
             </div>
-        </div>
         <?php
         endif;
         ?>
@@ -206,19 +210,19 @@ use yii\helpers\ArrayHelper;
         <?php
         //EGYEDI KERESÉS
         if ($params['q']):
-        ?>
-        <div class="desktop-filter">
-            <h4 class="filter-name">Egyedi keresés</h4>
-            <div class="imageButtonContainer">
-                <?php
-                echo Html::a(
-                    $params['q'],
-                    ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => $params['brand'], 'meret' => $params['meret'], 'szin' => $params['szin'], 'tipus' => $params['tipus'], 's' => $params['s']],
-                    ['class' => 'sizeButtonSelected']
-                );
-                ?>
+            ?>
+            <div class="desktop-filter">
+                <h4 class="filter-name">Egyedi keresés</h4>
+                <div class="imageButtonContainer">
+                    <?php
+                    echo Html::a(
+                        $params['q'],
+                        ['termekek/index', 'mainCategory' => $params['mainCategory'], 'subCategory' => $params['subCategory'], 'brand' => $params['brand'], 'meret' => $params['meret'], 'szin' => $params['szin'], 'tipus' => $params['tipus'], 's' => $params['s']],
+                        ['class' => 'sizeButtonSelected']
+                    );
+                    ?>
+                </div>
             </div>
-        </div>
         <?php
         endif;
         ?>
@@ -232,5 +236,5 @@ use yii\helpers\ArrayHelper;
                 ['class' => 'sizeButtonSelected delete-filter']
             );
         ?>
-            </div>
+    </div>
 </div>
