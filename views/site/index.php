83031b92ff3ec69ba1ae6c965b525a306c70b3b4
Aradvári Balázs <arad@Aradvaris-MacBook-Pro.local>
2018-09-14 22:21:32 +0200

    no message

--- views/site/index.php
+++ views/site/index.php
@@ -25,8 +25,7 @@ Yii::$app->seo->registerMetaTag(['name' => 'og:site_name', 'content' => 'Coresho
 Yii::$app->seo->registerMetaTag(['name' => 'article:section', 'content' => 'fashion']);
 Yii::$app->seo->registerMetaTag(['name' => 'article:tag', 'content' => $keywords]);
 Yii::$app->seo->registerMetaTag(['name' => 'fb:app_id', 'content' => '550827275293006']);
-
-//canonical
+//CANONICAL
 if (trim(Yii::$app->request->url, '/'))
     $this->registerLinkTag(['rel' => 'canonical', 'href' => Url::home(true)]);
 
@@ -36,21 +35,21 @@ if (trim(Yii::$app->request->url, '/'))
     <!-- Slider -->
     <div class="row d-none d-sm-block">
         <div id="coreshopCarousel" class="alice-blue-bg carousel slide" data-ride="carousel">
-            
-			<!-- //carousel-indicators
-			<ol class="carousel-indicators">
+
+            <!-- //carousel-indicators
+            <ol class="carousel-indicators">
                 <li data-target="#coreshopCarousel" data-slide-to="0" class="active"></li>
                 <li data-target="#coreshopCarousel" data-slide-to="1"></li>
                 <li data-target="#coreshopCarousel" data-slide-to="2"></li>
-            </ol> 
-			//endof carousel-indicators -->
+            </ol>
+            //endof carousel-indicators -->
 
             <!--Desktop slider -->
             <div class="carousel-inner">
-                <div class="carousel-item active">                    
-					<a href="/termekek?q=independent">
-                    <img class="d-block w-100" src="images/banner-carousel/2018/20180904-vans-x-independent.jpg" alt="Vans x Independent">
-					</a>
+                <div class="carousel-item active">
+                    <a href="/termekek?q=independent">
+                        <img class="d-block w-100" src="images/banner-carousel/2018/20180904-vans-x-independent.jpg" alt="Vans x Independent">
+                    </a>
                     <!-- <div class="carousel-caption d-none d-md-block">
                         <h5>Ultra <span class="blue">range</span></h5>
                         <p>Kényelem és lazaság</p>
@@ -58,7 +57,7 @@ if (trim(Yii::$app->request->url, '/'))
                 </div>
 
             </div> <!-- //carousel-inner -->
-			
+
 
             <!-- <a class="carousel-control-prev" href="#coreshopCarousel" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -75,7 +74,7 @@ if (trim(Yii::$app->request->url, '/'))
 
 
 
-   <!--Mobile slider -->
+    <!--Mobile slider -->
     <div class="row d-block d-sm-none">
         <div id="coreshopCarouselMobile" class="alice-blue-bg  carousel slide" data-ride="carousel">
 
@@ -85,13 +84,13 @@ if (trim(Yii::$app->request->url, '/'))
                     <div class="carousel-caption d-md-block">
                         <h5>Vans x Independent</h5>
                         <p>A Vans egyesíti erőit az Independent-tel, az egyik legkultikusabb gördeszka felfüggesztés gyártó céggel.
-						<br />
-						<br />Limitált kollekció, ne maradj le róla!</p>
+                            <br />
+                            <br />Limitált kollekció, ne maradj le róla!</p>
                         <a href="/termekek?q=independent" class="btn btn-primary">Megnézem</a>
                     </div>
                 </div>
 
-				<!-- 2nd item carousel mobile
+                <!-- 2nd item carousel mobile
                 <div class="carousel-item">
                     <img class="d-block w-100" src="images/slider/vans-ave_main-desk-chino-640x500.jpg" alt="First slide">
                     <div class="carousel-caption d-md-block">
@@ -100,19 +99,19 @@ if (trim(Yii::$app->request->url, '/'))
                         <a href="" class="btn btn-primary">Megnézem </a>
                     </div>
                 </div>
-				-->
+                -->
 
-                
+
             </div> <!-- //carousel-inner -->
-            
-			<div class="justify-content-center mt-3 d-flex">
+
+            <div class="justify-content-center mt-3 d-flex">
                 <ol class="carousel-indicators carousel-indicators-mobile">
-                        <!-- <li data-target="#coreshopCarouselMobile" data-slide-to="0" class="active"></li> -->
-                        <!-- <li data-target="#coreshopCarouselMobile" data-slide-to="1"></li> -->
-                </ol> 
+                    <!-- <li data-target="#coreshopCarouselMobile" data-slide-to="0" class="active"></li> -->
+                    <!-- <li data-target="#coreshopCarouselMobile" data-slide-to="1"></li> -->
+                </ol>
                 <!-- //carousel-indicators -->
             </div>
-			
+
             <!-- <a class="carousel-control-prev" href="#coreshopCarousel" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Előző</span>
@@ -131,63 +130,63 @@ if (trim(Yii::$app->request->url, '/'))
     <section class="container-fluid customer-logo-container">
         <div id="customerLogos" class="row alice-blue-bg">
 
-                <a class="col-md col-3 align-self-center" href="vans">
-                    <img class="img-fluid mx-auto brand-picker vans" src="images/markak/vans_logo.png" alt="Vans">
-                </a>
-                <a class="col-md col-3" href="etnies">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/etnies_logo.png" alt="Etnies">
-                </a>
-               
-                <a class="col-md col-3" href="emerica">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/emerica_logo.png" alt="Emerica">
-                </a>
-                <a class="col-md col-3" href="es">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/es_logo.png" alt="és">
-                </a>
-              
-                <a class="col-md col-3" href="neff">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/neff_logo.png" alt="Neff">
-                </a>
-                <a class="col-md col-3" href="powell-peralta">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/powell-peralta-logo.png" alt="Powell Peralta">
-                </a>
-                <a class="col-md col-3" href="volcom">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/volcom_logo.png" alt="Volcom">
-                </a>
-               <a class="col-md col-3" href="altamont">
-                    <img class="img-fluid mx-auto brand-picker" src="images/markak/altamont_logo.png" alt="Altamont">
-                </a>
-               
+            <a class="col-md col-3 align-self-center" href="vans">
+                <img class="img-fluid mx-auto brand-picker vans" src="images/markak/vans_logo.png" alt="Vans">
+            </a>
+            <a class="col-md col-3" href="etnies">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/etnies_logo.png" alt="Etnies">
+            </a>
+
+            <a class="col-md col-3" href="emerica">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/emerica_logo.png" alt="Emerica">
+            </a>
+            <a class="col-md col-3" href="es">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/es_logo.png" alt="és">
+            </a>
+
+            <a class="col-md col-3" href="neff">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/neff_logo.png" alt="Neff">
+            </a>
+            <a class="col-md col-3" href="powell-peralta">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/powell-peralta-logo.png" alt="Powell Peralta">
+            </a>
+            <a class="col-md col-3" href="volcom">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/volcom_logo.png" alt="Volcom">
+            </a>
+            <a class="col-md col-3" href="altamont">
+                <img class="img-fluid mx-auto brand-picker" src="images/markak/altamont_logo.png" alt="Altamont">
+            </a>
+
 
         </div>
     </section>
     <!-- //brands -->
 
-    
-	
-	
-	<!-- 1 -->
-	<section class="white-bg">
-        <div class="row">			
-			<div class="col-md col-12" style="padding:0;">
-			<a href="/cipo/ferfi-cipo/vans/sk8-hi-pro-blackwhite-7132"><img src="../images/banner-box/2018/20180905-sk8-hi-pro.jpg" style="width:100%;" alt="Vans Sk8-hi Pro cipő" /></a>
-            </div>	
-            
+
+
+
+    <!-- 1 -->
+    <section class="white-bg">
+        <div class="row">
+            <div class="col-md col-12" style="padding:0;">
+                <a href="/cipo/ferfi-cipo/vans/sk8-hi-pro-blackwhite-7132"><img src="../images/banner-box/2018/20180905-sk8-hi-pro.jpg" style="width:100%;" alt="Vans Sk8-hi Pro cipő" /></a>
+            </div>
+
             <div class="col-md col-12 align-self-center">
                 <h2 class="text-center">Vans Sk8-Hi <span class="blue">Pro</span></h2>
                 <p class="text-center">
-				Az SK8-Hi Pro a klasszikus Vans SK8-Hi felturbózott változata. Jellemzői az UltraCush HD talpbetét ami extra kényelmet biztosít, továbbá a Duracap megerősítés a fokozottan igénybe vett részeken, amely a tartósságért felelős. A felsőrész hasított bőr és textil kombinációja.
-				</p>
+                    Az SK8-Hi Pro a klasszikus Vans SK8-Hi felturbózott változata. Jellemzői az UltraCush HD talpbetét ami extra kényelmet biztosít, továbbá a Duracap megerősítés a fokozottan igénybe vett részeken, amely a tartósságért felelős. A felsőrész hasított bőr és textil kombinációja.
+                </p>
                 <p class="text-center margin-top-50">
                     <a href="/cipo/ferfi-cipo/vans/sk8-hi-pro-blackwhite-7132" class="btn btn-primary">Megnézem</a>
                 </p>
             </div>
         </div>
     </section>
-	
-	
-	<!-- 2 -->
-	<section>
+
+
+    <!-- 2 -->
+    <section>
         <div class="row blue-bg">
             <div class="col-md col-12 align-self-center order-1">
                 <h2 class="text-center inverse">Vans Ultrarange</h2>
@@ -198,70 +197,70 @@ if (trim(Yii::$app->request->url, '/'))
                 </p>
             </div>
             <div class="col-md-5 order-md-2 col-12" style="padding:0;">
-			<a href="/cipo?q=ultrarange"><img src="../images/banner-box/2018/20180905-vans-ultrarange.jpg" style="width:100%;" alt="Vans Ultrarange cipő" /></a>
+                <a href="/cipo?q=ultrarange"><img src="../images/banner-box/2018/20180905-vans-ultrarange.jpg" style="width:100%;" alt="Vans Ultrarange cipő" /></a>
             </div>
         </div>
     </section>
-	
-	<!-- 3 -->
+
+    <!-- 3 -->
     <section class="white-bg">
         <div class="row">
             <div class="col-md col-12" style="padding:0;">
-			<a href="/powell-peralta"><img src="../images/banner-box/2018/20180905-powell-peralta-logo.jpg" style="width:100%;" alt="Powell Peralta" /></a>
+                <a href="/powell-peralta"><img src="../images/banner-box/2018/20180905-powell-peralta-logo.jpg" style="width:100%;" alt="Powell Peralta" /></a>
             </div>
-			
+
             <div class="col-md col-12 align-self-center">
                 <h2 class="text-center">Powell <span class="blue">Peralta</span></h2>
                 <p class="text-center">Pólóújdonságok Santa Barbara, Kaliforniából! Az egyik legkultikusabb gördeszka gyártótól pólók, komplett deszkák, gördeszka lapok és kerekek érkeztek kínálatunkba.
-					</p>
+                </p>
                 <p class="text-center margin-top-50">
                     <a href="/powell-peralta" class="btn btn-primary">Megnézem</a>
                 </p>
             </div>
         </div>
     </section>
-	
-	
-	
-	
-	
-<div class="alice-blue-bg m--15">
-    <div class="container">
+
+
+
+
+
+    <div class="alice-blue-bg m--15">
+        <div class="container">
             <div class="row justify-content-center">
                 <h2 class="mt-5 my-5 text-center"> Legújabb <span class="blue">termékek</span> </h2>
             </div>
+        </div>
+
+        <?php
+        // echo '<h2 class="mt-5 my-5 text-center"> Legújabb <span class="blue">termékek</span> </h2>';
+        $dataProvider = (new TermekekSearch())->search(['subCategory' => ['ferfi-cipo', 'noi-cipo']]);
+        $dataProvider->pagination = false;
+        $dataProvider->query->andWhere(['opcio' => 'UJ']);
+        $dataProvider->query->limit(12);
+        $dataProvider->query->orderBy('rand()');
+
+        if ($dataProvider->getCount() > 0)
+            echo $this->render('/termekek/_index_ajanlo', ['dataProvider' => $dataProvider]);
+        ?>
     </div>
 
-<?php
-// echo '<h2 class="mt-5 my-5 text-center"> Legújabb <span class="blue">termékek</span> </h2>';
-$dataProvider = (new TermekekSearch())->search(['subCategory' => ['ferfi-cipo', 'noi-cipo']]);
-$dataProvider->pagination = false;
-$dataProvider->query->andWhere(['opcio' => 'UJ']);
-$dataProvider->query->limit(12);
-$dataProvider->query->orderBy('rand()');
-
-if ($dataProvider->getCount() > 0)
-    echo $this->render('/termekek/_index_ajanlo', ['dataProvider' => $dataProvider]);
-?>
-</div>
-
-<div class="alice-blue-bg m--15">
-    <div class="container" style="padding-bottom:50px;">
+    <div class="alice-blue-bg m--15">
+        <div class="container" style="padding-bottom:50px;">
             <div class="row justify-content-center">
                 <h2 class="mt-5 my-5 text-center">Rólunk</h2>
             </div>
-    
-	Köszönjük, hogy benéztél, ha már itt vagy akkor szeretnénk megosztani veled egy kis Coreshop történelmet.
-	<br />
-	<br />
-	Ott kezdődött minden, ahol a nap nyugszik, a nyugati parton, Dél-Kaliforniában.  Ahol olyan termékeket készítenek, amelyeket szívesen hordunk, és bő szívvel ajánljuk mindenki másnak is. 
-	<br />
-	<br />
-	Cégünk 2007. májusában kezdte meg működését, mely az első webshop volt a kategóriájában. Akkor még Zoneshop néven kínáltuk a gördeszkás cipőket, textileket és hardvereket, majd 2009. őszétől Coreshop néven folytattuk - utalva rá, hogy Magyarországon mi voltunk ebben a kategóriában a mag, de ennek a teljes története egészen a '90-es évekig nyúlik vissza. Megalakulásunk óta kényesen ügyelünk arra, hogy termékválasztékban és szolgáltatásban is kompromisszum-mentes minőséget kínáljunk. Szezononként újra és újra, a vásárlói igények alapján válogatjuk össze a legjobb márkák (Vans, etnies, éS, Emerica) legjobb termékeit.
-	<br />
-	<br />
-	Az elmúlt közel tíz év alatt több ezer elégedett vásárló visszajelzése igazolja, hogy jó úton haladunk.
-	</div>
-</div>
+
+            Köszönjük, hogy benéztél, ha már itt vagy akkor szeretnénk megosztani veled egy kis Coreshop történelmet.
+            <br />
+            <br />
+            Ott kezdődött minden, ahol a nap nyugszik, a nyugati parton, Dél-Kaliforniában.  Ahol olyan termékeket készítenek, amelyeket szívesen hordunk, és bő szívvel ajánljuk mindenki másnak is.
+            <br />
+            <br />
+            Cégünk 2007. májusában kezdte meg működését, mely az első webshop volt a kategóriájában. Akkor még Zoneshop néven kínáltuk a gördeszkás cipőket, textileket és hardvereket, majd 2009. őszétől Coreshop néven folytattuk - utalva rá, hogy Magyarországon mi voltunk ebben a kategóriában a mag, de ennek a teljes története egészen a '90-es évekig nyúlik vissza. Megalakulásunk óta kényesen ügyelünk arra, hogy termékválasztékban és szolgáltatásban is kompromisszum-mentes minőséget kínáljunk. Szezononként újra és újra, a vásárlói igények alapján válogatjuk össze a legjobb márkák (Vans, etnies, éS, Emerica) legjobb termékeit.
+            <br />
+            <br />
+            Az elmúlt közel tíz év alatt több ezer elégedett vásárló visszajelzése igazolja, hogy jó úton haladunk.
+        </div>
+    </div>
 
 </div>
